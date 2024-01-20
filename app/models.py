7cf9549038b4ae29490a64fe72759143134b56e9
Abolo Samuel <ikabolo59@gmail.com>
2024-01-20 12:38:32 +0100

    Pydantic models needed for the tailor system

---
+++ app/models.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from typing import (
+    List,
+    Optional
+)
+
+from pydantic import (
+    BaseModel,
+    Field
+)
+
+from uuid import (
+    UUID,
+    uuid4
+)
+
+
+class LoginForm(BaseModel):
+
+    email: str
+    password: str
+
+
+class OurBase(BaseModel):
+
+    id: UUID = Field(default=uuid4)
+
+
+class User(BaseModel):
+
+    name: str
+    email: str
+    password: str
+    gender: str
+
+
+class Product(OurBase):
+
+    name: str
+    description: str
+    price: float
+
+
+class Tailor(User):
+
+    bio: str
+    experience_level: str
+    
+    location: str
+
+    cost: float
+
+    product: List[Product]
+
+
+class Order(OurBase):
+
+    class Tracking(OurBase):
+
+        title: str
+        description: str
+        date: datetime 
+        status: str
+
+    user_id: UUID
+    tailor_id: UUID
+
+    product_id: Optional[UUID]
+
+    price_agreed: float
+
+    due_date: datetime 
+
+    status: str
+
+    tracking: List[Tracking]
