4645a0738314d4d1ed1b13ebbb5a791dfcf14349
Abolo Samuel <ikabolo59@gmail.com>
2024-03-26 08:39:11 +0100

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI 
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from app.routers import (
     lazy_router
 )
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 app.include_router(lazy_router.router)
